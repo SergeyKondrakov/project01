9ab0439d4c0dea0b156e50838f92f6390d82bddc
SergeyKondrakov <sergey.kondrakov@gmail.com>
2021-02-15 20:50:48 +0300

    various css added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Profile.css';
 
 function Profile() {
     return <div className='content'>
@@ -8,11 +9,11 @@ function Profile() {
           
       <div className='profilewrapper'>
         <div>
-          <img className='profilepic' src='https://profile.radidomapro.ru/images/membres/auto/165-165-r/00b64b0cee5d61954958564cd2858991_20170921132048.png'></img>
+          <img className='profilepic' src='https://profile.radidomapro.ru/images/membres/auto/165-165-r/436b6b78c41d810d8750b43540a5e129_20181023164748.png'></img>
         </div>
         <div className='userprofile'>
-          <p>Анастасия Клименкова</p>
-          <span>Менеджер по B2B-продвижению</span><br></br>
+          <p>Сергей Кондраков</p>
+          <span>Главный редактор</span><br></br>
           <span>ООО "Кап Инфо Про"</span>
         </div>
       </div>
